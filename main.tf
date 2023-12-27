aa688db539967efc7a49076c9ad9282a48d94234
Thangavelu@ <chakraodba@gmail.com>
2023-12-27 12:42:46 +0530

    formatted

--- main.tf
+++ main.tf
@@ -1,25 +1,21 @@
 #adding this comment
 terraform {
   backend "s3" {
-    bucket = var.bucket_name
+    bucket_name = var.bucket_name
     #key     = "django/statefile.tfstate"
-    key     = var.bucket_key
-    region  = local.aws_region
-    encrypt = var.need_encrypt
+    bucket_key     = var.bucket_key
+    need_encrypt = var.need_encrypt
   }
 }
 
 module "ec2_for_django" {
   source = "git::https://github.com/chakratechgeek/djangoAwsInfra-BP.git//ec2"
 
-  bucket_name      = var.bucket_name
-  bucket_key       = var.bucket_key
   vpc_id           = var.vpc_id
   subnet_id        = var.subnet_id
   ec_ami           = var.ec_ami
   instance_type    = var.instance_type
   key_name         = var.key_name
-  need_encrypt     = var.need_encrypt
   pub_ip_associate = var.pub_ip_associate
 
 }
