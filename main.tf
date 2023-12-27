722e424e96d244a2e9055df13f8fbee37f6c2d41
Thangavelu@ <chakraodba@gmail.com>
2023-12-27 12:24:55 +0530

    adding parameters

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "ec2_for_django" {
+  source = "git::https://github.com/organization-name/terraform-modules.git//ec2"
+
+  bucket_name      = var.bucket_name
+  bucket_key       = var.bucket_key
+  vpc_id           = var.vpc_id
+  subnet_id        = var.subnet_id
+  ec_ami           = var.ec_ami
+  instance_type    = var.instance_type
+  key_name         = var.key_name
+  need_encrypt     = var.need_encrypt
+  pub_ip_associate = var.pub_ip_associate
+
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "dummy" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "bucket_key" {
+  type = string
+
+}
+
+variable "vpc_id" {
+  type = string
+
+}
+
+variable "subnet_id" {
+  type = string
+
+}
+
+variable "ec_ami" {
+  type = string
+
+}
+
+variable "instance_type" {
+  type = string
+
+}
+
+variable "key_name" {
+  type = string
+
+}
+
+variable "need_encrypt" {
+  type    = bool
+  default = true
+
+}
+
+variable "pub_ip_associate" {
+  type    = bool
+  default = false
+}
