38937268f0dd27ba061801ed34085d7a9c2c63ed
Thangavelu@ <chakraodba@gmail.com>
2023-12-27 13:04:26 +0530

    formatted

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+locals {
+  # Example: AWS region
+  aws_region   = "ap-south-1"
+  bucket_name  = "temp1985"
+  bucket_key   = "django/statefile.tfstate"
+  need_encrypt = true
+}
+
 #adding this comment
 terraform {
   backend "s3" {
